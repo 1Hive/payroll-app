bfe3260155c61cd6c309158b69eb102a904ec176
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-05-04 21:16:30 -0300

    Update app logic functionality

---
+++ buidler-app/app/src/app-state-reducer.js
@@ -0,0 +1,35 @@
+import BN from 'bn.js'
+
+function appStateReducer(state) {
+  if (state === null) {
+    return { isSyncing: true }
+  }
+
+  const { employees, payments, pctBase } = state
+
+  return {
+    ...state,
+
+    numData: {
+      pctBase: parseInt(pctBase, 10),
+    },
+
+    employees: employees?.map(({ accruedSalary, salary, ...employee }) => ({
+      ...employee,
+      accruedSalary: new BN(accruedSalary),
+      salary: new BN(salary),
+    })),
+
+    pctBase: new BN(pctBase.toString()), 
+    payments:
+      payments?.map(
+        ({ denominationAllocation, denominationAmount, ...payment }) => ({
+          ...payment,
+          denominationAllocation: new BN(denominationAllocation),
+          denominationAmount: new BN(denominationAmount),
+        })
+      ) || [],
+  }
+}
+
+export default appStateReducer
