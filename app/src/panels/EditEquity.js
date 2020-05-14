c17d02e9216782318c5209f834872f377e39a118
Cristopher Ortega <ortega.cpp@gmail.com>
2020-05-14 04:15:52 -0400

    EditEquityOption: finished implementing UI design

--- app/src/panels/EditEquity.js
+++ app/src/panels/EditEquity.js
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react'
-import { SidePanel } from '@aragon/ui'
+import { Button, Field, TextInput, GU, SidePanel } from '@aragon/ui'
 
 const EditEquity = React.memo(function EditEquity({ editEquityOptionPanel }) {
   const handleClose = useCallback(() => {
@@ -7,13 +7,61 @@ const EditEquity = React.memo(function EditEquity({ editEquityOptionPanel }) {
   }, [editEquityOptionPanel])
   return (
     <SidePanel
-      title="Edit Equity"
+      title="Edit Equity Option"
       opened={editEquityOptionPanel && editEquityOptionPanel.visible}
       onClose={handleClose}
     >
-      <div />
+      <EditEquityContent />
     </SidePanel>
   )
 })
+
+function EditEquityContent() {
+  const handleSubmit = useCallback(() => {
+    // some logic goes here
+  }, [])
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field
+        css={`
+          margin-top: ${3 * GU}px;
+          max-width: 100%;
+        `}
+        label="Multiplier"
+      >
+        <TextInput wide />
+      </Field>
+      <Field
+        css={`
+          margin-top: ${3 * GU}px;
+          max-width: 100%;
+        `}
+        label="Vesting period (in months)"
+      >
+        <TextInput wide />
+      </Field>
+      <Field
+        css={`
+          margin-top: ${3 * GU}px;
+          max-width: 100%;
+        `}
+        label="Vesting cliff (in months)"
+      >
+        <TextInput wide />
+      </Field>
+
+      <Button
+        css={`
+          margin-top: ${2 * GU}px;
+        `}
+        label="Save changes"
+        mode="strong"
+        wide
+        type="submit"
+      />
+    </form>
+  )
+}
 
 export default EditEquity
